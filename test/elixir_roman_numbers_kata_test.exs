523334a03fa172b1afa70616f6eff71a410e07d8
Daniele Megna <megna.dany@gmail.com>
2015-08-26 22:29:00 +0200

    Removed duplication in tests

--- test/elixir_roman_numbers_kata_test.exs
+++ test/elixir_roman_numbers_kata_test.exs
@@ -2,27 +2,31 @@ defmodule ElixirRomanNumbersKataTest do
   use ExUnit.Case
 
   test "should return I with 1" do
-    assert Arabic.to_roman(1) == "I"
+    assertArabicToRoman(1, "I")
   end
 
   test "should return II with 2" do
-    assert Arabic.to_roman(2) == "II"
+    assertArabicToRoman(2, "II")
   end
 
   test "should return III with 3" do
-    assert Arabic.to_roman(3) == "III"
+    assertArabicToRoman(3, "III")
   end
 
   test "should return V with 5" do
-    assert Arabic.to_roman(5) == "V"
+    assertArabicToRoman(5, "V")
   end
   
   test "should return VI with 6" do
-    assert Arabic.to_roman(6) == "VI"
+    assertArabicToRoman(6, "VI")
   end
 
   test "should return VII with 7" do
-    assert Arabic.to_roman(7) == "VII"
+    assertArabicToRoman(7, "VII")
+  end
+
+  defp assertArabicToRoman(arabic, roman) do
+    assert Arabic.to_roman(arabic) == roman
   end
 
 end
