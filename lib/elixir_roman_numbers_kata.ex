4c15be0ceefee0e2f3440874489d2d70d8f26e8b
Daniele Megna <megna.dany@gmail.com>
2015-08-26 22:50:27 +0200

    Changed string build strategy

--- lib/elixir_roman_numbers_kata.ex
+++ lib/elixir_roman_numbers_kata.ex
@@ -3,7 +3,8 @@ defmodule Arabic do
   def to_roman(1), do: "I"
   def to_roman(5), do: "V"
   def to_roman(10), do: "X"
-  def to_roman(n) when n >= 15, do: to_roman(n-5) <> to_roman(5)
-  def to_roman(n), do: to_roman(n-1) <> to_roman(1)
+  def to_roman(n) when n > 10,  do: to_roman(10)  <> to_roman(n-10)
+  def to_roman(n) when n > 5,   do: to_roman(5)   <> to_roman(n-5)
+  def to_roman(n) when n > 1,   do: to_roman(1)   <> to_roman(n-1)
 
 end
