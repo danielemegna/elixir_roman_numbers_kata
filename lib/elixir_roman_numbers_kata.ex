36fb782ba6edb9fe03031170113b414fdc0efe7b
Daniele Megna <megna.dany@gmail.com>
2015-08-26 23:21:41 +0200

    Introducing dictionary map

--- lib/elixir_roman_numbers_kata.ex
+++ lib/elixir_roman_numbers_kata.ex
@@ -1,9 +1,16 @@
 defmodule Arabic do
 
-  def to_roman(1), do: "I"
-  def to_roman(4), do: "IV"
-  def to_roman(5), do: "V"
-  def to_roman(10), do: "X"
+  @dictionary %{
+    1 => "I",
+    4 => "IV",
+    5 => "V",
+    10 => "X",
+  }
+
+  def to_roman(1), do: @dictionary[1]
+  def to_roman(4), do: @dictionary[4]
+  def to_roman(5), do: @dictionary[5]
+  def to_roman(10), do: @dictionary[10]
   def to_roman(n) when n > 10,  do: to_roman(10)  <> to_roman(n-10)
   def to_roman(n) when n > 5,   do: to_roman(5)   <> to_roman(n-5)
   def to_roman(n) when n > 1,   do: to_roman(1)   <> to_roman(n-1)
